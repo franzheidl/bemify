a39ffcfe6ae0e676b976b5ec72fc05141c89df2a
Franz Heidl <franz@franzheidl.de>
2015-05-25 18:38:16 +0200

    Add gh-page source

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,18 +15,66 @@ module.exports = function(grunt) {
         files: {
           './tmp/test.css': 'test/test.scss'
         }
+      },
+      site: {
+        options: {
+          style: 'compressed',
+          compass: true,
+          sourcemap: 'none'
+        },
+        files: {
+          'main.css': 'gh-page/src/sass/main.scss'
+        }
+      }
+    },
+    
+    jade: {
+      compile: {
+        files: {
+          './index.html': 'gh-page/src/jade/index.jade'
+        }
+      }
+    },
+    
+    browserSync: {
+      site: {
+        bsFiles: {
+          src: ['./*.css', './*.html']
+        },
+        options: {
+          server: {
+            baseDir: './'
+          },
+          watchTask: true,
+          port: 8080
+        }
       }
     },
     
     watch: {
       sass: {
-        files: ['sass/*.scss', 'test/**/*.scss'],
-        tasks: ['sass:compile']
+        files: ['gh-page/src/sass/**/*.scss'],
+        tasks: ['sass:site']
+      },
+      jade: {
+        files: ['gh-page/src/jade/index.jade'],
+        tasks: ['jade:compile']
       }
+    },
+    
+    'gh-pages': {
+      options: {
+        base: './'
+      },
+      src: ['index.html', 'main.css', 'favicon.ico']
     }
     
   });
-
   
-  grunt.registerTask('default', ['watch']);
+  
+  grunt.registerTask('compile', ['sass:compile']);
+  grunt.registerTask('build', ['sass:site', 'jade:compile'])
+  grunt.registerTask('default', ['build', 'browserSync', 'watch']);
+  grunt.registerTask('site', ['gh-pages']);
+  
 };
